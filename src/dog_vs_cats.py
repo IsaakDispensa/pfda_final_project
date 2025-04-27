981e282202d737b07cfd215be1030296f29182f7
miadi <dispensa24@gmail.com>
2025-04-26 20:19:28 -0500

    Initialized project with basic setup and constants.

---
+++ src/dog_vs_cats.py
@@ -0,0 +1,16 @@
+import pygame
+import random
+import time
+
+#Constants
+GAME_WIDTH, GAME_HEIGHT = 1400, 900
+DOG_STEP = 5.0
+BALL_SPEED = 4.0
+CAT_SPAWN_INTERVAL = 2
+CAT_SPEED = 2.0
+
+# Initialize Pygame
+pygame.init()
+
+
+
